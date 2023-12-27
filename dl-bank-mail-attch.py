d438f306701c09cac55c19ec9c5be385b985c854
uWayLu <u.master.o.twn@gmail.com>
2023-12-27 16:33:16 +0800

    feat: release main feat

--- dl-bank-mail-attch.py
+++ dl-bank-mail-attch.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 from dotenv import load_dotenv
+
 load_dotenv(verbose=True)
 
 import functools
@@ -15,12 +16,21 @@ mail_password = os.getenv('GMAIL_PW')
 mail_folder = os.getenv('GMAIL_FOLDER')
 
 # config
-download_dir = os.getenv('DOWNLOAD_DIR') + '/tests'
+download_dir = os.path.join(os.getenv('DOWNLOAD_DIR'), mail_user.split('@')[0])
+
+if not os.path.exists(download_dir):
+    try:
+        os.mkdir(download_dir)
+        print('folder %s created' % download_dir)
+    except FileExistsError:
+        print('folder %s already exists' % download_dir)
+
 
 def decode_mime_words(s):
     return ''.join(
-        word.decode(encoding or 'utf8') for word, encoding in email.header.decode_header(s)
-)
+        word.decode(encoding or 'utf8')
+        for word, encoding in email.header.decode_header(s))
+
 
 # 連接到郵件伺服器
 M = imaplib.IMAP4_SSL(mail_server)
@@ -29,40 +39,47 @@ M.select(mail_folder)
 
 # TODO 搜尋主旨，儲存檔名(發信日-)
 
+
 def dl_bank_mail_attch(num):
     typ, msg_data = M.fetch(num, '(RFC822)')
     email_message = email.message_from_bytes(msg_data[0][1])
 
     # 獲取郵件主旨
-    subject = email_message['Subject']
-    print(num.decode(), decode_mime_words(subject))
-    # print('永豐銀*' in decode_mime_words(subject))
-    M.store(num, '-FLAGS', '(\Seen)')
+    subject = decode_mime_words(email_message['Subject'])
+    print(num.decode(), subject)
+    # M.store(num, '-FLAGS', '(\Seen)')
 
     # 是否包含附件
     has_attch = False
     if email_message.get_content_maintype() == 'multipart':
         has_attch = functools.reduce(
-            lambda a, b: b and a,
-            (part.get_content_disposition() is None for part in email_message.walk()),
-            True
-        )
-    return has_attch
+            lambda a, b: b or a, (part.get_content_disposition() is not None
+                                  for part in email_message.walk()), False)
+
+    if not has_attch: return has_attch
 
     # 如果主旨符合特定條件
-    # if '特定主旨關鍵字' in subject:
-    #     # 下載附件
-    #     for part in email_message.walk():
-    #         if part.get_content_maintype() == 'multipart':
-    #             continue
-    #         if part.get('Content-Disposition') is None:
-    #             continue
-    #         filename = part.get_filename()
-    #         if filename:
-    #             filepath = os.path.join(download_dir, filename)
-    #             if not os.path.isfile(filepath):
-    #                 with open(filepath, 'wb') as f:
-    #                     f.write(part.get_payload(decode=True))
+    if '帳單' in subject:
+        print('  >', 'Checking attachments of %s' % subject)
+        # 下載附件
+        for part in email_message.walk():
+            if part.get_content_maintype() == 'multipart':
+                continue
+            if part.get('Content-Disposition') is None:
+                continue
+            try:
+                filename = decode_mime_words(part.get_filename())
+            except:
+                filename = part.get_filename()
+            if filename:
+                filepath = os.path.join(download_dir, '%s-%s-%s' % (num.decode(), subject, filename))
+                if not os.path.isfile(filepath):
+                    with open(filepath, 'wb') as f:
+                        f.write(part.get_payload(decode=True))
+                        print('  >', 'Downloaded to %s' % filepath)
+                else:
+                    print('  >', '%s already exists' % filepath)
+        M.store(num, '-FLAGS', '(\Seen)')
 
 # TODO 移除附件密碼
 
@@ -72,16 +89,12 @@ iter = 0
 
 # 遍歷所有未讀郵件
 for num in reversed(data[0].split()):
-    if dl_bank_mail_attch(num): 
+    if dl_bank_mail_attch(num):
+        iter += 1
+    if iter < 0:
+        print(iter)
         break
 
 # 關閉郵件伺服器連接
 M.close()
 M.logout()
-
-
-def main():
-    print ('Hello, World!')
-
-
-# main()
